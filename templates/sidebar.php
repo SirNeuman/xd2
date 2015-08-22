b9fbd70b8aaef0211541be2474bf28c416e3ed35
Nicholas Toblski <mrtobo@gmail.com>
2015-08-21 20:57:46 -0400

    Fix nav colors and blockqoute size.

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,10 +1,9 @@
 <?php  
-if( is_single() || is_post_type_archive() || is_home() || is_author() || is_category() || is_date())
-{
-    dynamic_sidebar('sidebar-blog');
+if( is_single() || is_post_type_archive() || is_home() || is_author() || is_category() || is_date()) {
+  dynamic_sidebar('sidebar-blog');
 } else if (is_page_template( 'template-angular-app.php')) {
-  dynamic_sidebar('sidebar-idea-coud');
+  dynamic_sidebar('sidebar-primary');
 } else {
-    dynamic_sidebar('sidebar-primary');
+  dynamic_sidebar('sidebar-primary');
 }
 ?>
